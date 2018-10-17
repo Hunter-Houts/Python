99bbbd03c788ecca3b6c40507ca867794d3486da
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-10-16 21:44:46 -0500

    PIP 8

--- Assessment in python.py
+++ Assessment in python.py
@@ -1,65 +1,72 @@
-def isTen(num):
-    if(num == 10):
+def is_ten(num):
+    if num == 10:
         return True
     else:
         return False
+
+
 def double(num):
     return num + num
+
+
 def remove9s(arr):
-    newArr = []
+    new_arr = []
     for i in range(len(arr)):
         if arr[i] != 9:
-            newArr.append(arr[i])
-    return newArr
-        
-def capitalizeName(str):
+            new_arr.append(arr[i])
+    return new_arr
+
+
+def capitalize_name(str):
     return str.title()
-def capitalizeAllNames(arr):
+
+
+def capitalize_all_names(arr):
     for i in range(len(arr)):
         if i < len(arr):
             print(arr[i].title())
             i += 1
 
-        
 
-def countVowels(str):
+def count_vowels(str):
     str.lower()
     a = str.count("a")
     e = str.count("e")
     i = str.count("i")
     o = str.count("o")
     u = str.count("u")
-    totalCount = a+e+i+o+u
-    return totalCount
-def averageSales(arr):
-    lenth = len(arr)
+    total_count = a + e + i + o + u
+    return total_count
+
+
+def average_sales(arr):
     sales = 0
-    i = 0
-    for i in length:
+    for i in range(len(arr)):
         sales += arr[i].sales
-        i += 1
-    return sales/length
-def analyzeWord(str):
+
+    return sales / len(arr)
+
+
+def analyze_word(str):
     class obj:
         name = str
         numberOfLetters = len(str)
-        numberOfVowels = countVowels(str.lower())
-    
+        numberOfVowels = count_vowels(str.lower())
+
     return [obj.name, obj.numberOfLetters, obj.numberOfVowels]
-def analyzeAllWords(arr):
+
+
+def analyze_all_words(arr):
     newArr = []
     for i in range(len(arr)):
-        newArr.append(analyzeWord(arr[i]))
+        newArr.append(analyze_word(arr[i]))
         i += 1
     return newArr
-def padArray(arr, length, fill):
+
+
+def pad_array(arr, length, fill):
     for i in range(len(arr)):
-        if i <= length and i >= length == False:
+        if i <= length and not i >= length:
             arr = arr.append(fill)
             i += 1
         return arr
-        
-        
-        
-    
-    
